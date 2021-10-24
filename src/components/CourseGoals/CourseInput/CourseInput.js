b8edc7a8a73b23489e297233ca4084cccdbd6e01
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-10-24 11:24:04 -0500

    yarn + removed ability to add empty goal

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import "./CourseInput.css";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState("");
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -12,6 +13,9 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
